299ca50cc9833e1e8608d0c645b61157e701509d
Jack Jennings <jack@pokalo.net>
2013-04-06 12:52:33 -0400

    allow versioning with info.plist

--- Mechanic.roboFontExt/lib/mechanic/models.py
+++ Mechanic.roboFontExt/lib/mechanic/models.py
@@ -4,7 +4,7 @@ from glob import glob
 import requests
 from zipfile import ZipFile
 
-from mojo.extensions import ExtensionBundle, getExtensionDefault, setExtensionDefault
+from mojo.extensions import ExtensionBundle
 from mechanic.helpers import Version
 
 class Extension(object):
@@ -23,8 +23,11 @@ class Extension(object):
         if(os.path.exists(self.configPath)):
             self.config = plistlib.readPlist(self.configPath)
             if 'repository' in self.config:
+                extension_path = self.config['extensionPath'] if hasattr(self.config, 'extensionPath') else None
+                self.remote = GithubRepo(self.config['repository'], 
+                                         name=self.name,
+                                         extension_path=extension_path)
                 self.configured = True
-                self.remote = GithubRepo(self.config['repository'], name=self.name)
             
     def update(self, extension_path=None):
         """Download and install the latest version of the extension."""
@@ -44,38 +47,43 @@ class Extension(object):
 
 class GithubRepo(object):
     
-    tags_url = "https://api.github.com/repos/%s/tags"
+    tags_url = "https://api.github.com/repos/%(repo)s/tags"
+    zip_url = "https://github.com/%(repo)s/archive/master.zip"
+    plist_url = "https://raw.github.com/%(repo)s/master/%(plist_path)s"
 
-    def __init__(self, repo, name=None):
+    def __init__(self, repo, name=None, extension_path=None):
         self.repo = repo
-        self.name = name
+        self.extension_path = extension_path
         self.username, self.name = repo.split('/')
+        if name is not None:
+            self.name = name
         self.version = None
-        self.downloading = False
-        self.chunk_count = None
-        self.download_percentage = 0.0
-        self.tmp_path = os.path.join("/", "tmp", "Mechanic", self.repo)
         
     def get(self):
         """Return the version and location of remote extension."""
-        response = requests.get(self.tags_url % self.repo)
-        response.raise_for_status()
-        self.tags = response.json()
-        try:
+        if self.extension_path:
+            plist_path = os.path.join(self.extension_path, 'info.plist')
+            plist_url = self.plist_url % {'repo': self.repo, 'plist_path': plist_path}
+            response = requests.get(plist_url)
+            plist = plistlib.readPlistFromString(response.content)
+            self.zip = self.zip_url % {'repo': self.repo}
+            self.version = plist['version']
+        elif self._get_tags():
             self.tags.sort(key=lambda s: list(Version(s["name"])), reverse=True)
-            self.data = self.tags[0]
-            self.version = self.data['name']
-        except:
-            self.data = False
-        return self.data
-        
+            self.zip = self.tags[0]['zipball_url']
+            self.version = self.tags[0]['name']
+        else:
+            self.zip = self.zip_url % {'repo': self.repo}
+    
     def setup_download(self):
         """Clear extension tmp dir, open download stream and local file."""
         if not hasattr(self,'data'):
             self.get()
+        self.tmp_path = os.path.join("/", "tmp", "Mechanic", self.repo)
         self._flush_tmp_path()
-        self.file = open(os.path.join(self.tmp_path, "%s.zip" % os.path.basename(self.data['zipball_url'])), "wb")
-        self.stream = requests.get(self.data['zipball_url'], stream=True)
+        filepath = os.path.join(self.tmp_path, "%s.zip" % os.path.basename(self.zip))
+        self.file = open(filepath, "wb")
+        self.stream = requests.get(self.zip, stream=True)
         self.stream_content = self.stream.iter_content(chunk_size=8192)
         self.content_length = self.stream.headers['content-length']
     
@@ -102,6 +110,12 @@ class GithubRepo(object):
             self.file.write(content)
         self.file.close()
         return self.extract_file()
+
+    def _get_tags(self):
+        response = requests.get(self.tags_url % {'repo': self.repo})
+        response.raise_for_status()
+        self.tags = response.json()
+        return self.tags
 
     def _flush_tmp_path(self):
         if os.path.exists(self.tmp_path):
