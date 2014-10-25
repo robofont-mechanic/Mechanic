c21021440f387f8c3d483556f7fc9bbaf197737e
Jack Jennings <j@ckjennin.gs>
2014-10-25 13:44:23 -0700

    implement cached response

--- src/lib/mechanic/cached_request.py
+++ src/lib/mechanic/cached_request.py
@@ -9,6 +9,28 @@ class CachedRequest(object):
         self.url = url
 
     def get(self):
-        response = requests.get(self.url)
+        return self.cache_response(self.url, self.get_cached(self.url))
+
+    def get_cached(self, url):
+        cached_response = self.cache.get(url, None)
+        if cached_response is not None:
+            etag = self.get_etag(cached_response)
+            response = requests.get(url, headers={'If-None-Match': etag})
+            if response.status_code is 304:
+                response = cached_response
+        else:
+            response = requests.get(url)
         response.raise_for_status()
         return response
+
+    def cache_response(self, url, response):
+        if self.get_etag(response):
+            self.cache[url] = response
+        return response
+
+    def get_etag(self, response):
+        return response.headers['ETag']
+
+    @property
+    def cache(self):
+        return self.__class__.__cache
