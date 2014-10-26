dfd9f4742a04a8ec1e5adea22a75c166782054de
Jack Jennings <j@ckjennin.gs>
2014-10-25 19:09:47 -0700

    simply registry url

--- src/lib/_startup.py
+++ src/lib/_startup.py
@@ -1,3 +1,4 @@
+from mechanic import default_registry
 from mechanic.storage import Storage
 from mechanic.observer import UpdateObserver
 
@@ -6,7 +7,8 @@ Storage.set_defaults(ignore={},
                      check_on_startup=True,
                      cache={},
                      cached_at=0.0,
-                     ignore_patch_updates=False)
+                     ignore_patch_updates=False,
+                     registries=[default_registry])
 
 UpdateObserver('applicationDidFinishLaunching',
                'applicationDidBecomeActive')
--- src/lib/mechanic/registry.py
+++ src/lib/mechanic/registry.py
@@ -1,18 +1,19 @@
 import requests
 
+from mechanic.storage import Storage
+
 
 class Registry(object):
-    registry_url = "http://www.robofontmechanic.com/api/v1/registry.json"
-
-    def __init__(self, url=None):
-        if url is not None:
-            self.registry_url = url
 
     def all(self):
-        response = requests.get(self.registry_url)
+        response = requests.get(self.url)
         response.raise_for_status()
         return response.json()
 
     def add(self, **data):
-        response = requests.post(self.registry_url, data=data)
+        response = requests.post(self.url, data=data)
         return response
+
+    @property
+    def url(self):
+        return Storage.get('registries')[0]
