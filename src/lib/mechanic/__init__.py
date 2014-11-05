ffd19c8a77f6c0e158c941f6303c6b585c39a65e
Jack Jennings <j@ckjennin.gs>
2014-11-04 17:21:13 -0800

    remove obsolete vars

--- src/lib/mechanic/__init__.py
+++ src/lib/mechanic/__init__.py
@@ -6,7 +6,3 @@ mechanic_path = os.path.dirname(__file__)
 lib_path = os.path.abspath(os.path.join(mechanic_path, "..", "packages"))
 if lib_path not in sys.path:
     sys.path.append(lib_path)
-
-__all__ = ["models", "views", "helpers"]
-
-default_registry = "http://www.robofontmechanic.com"
