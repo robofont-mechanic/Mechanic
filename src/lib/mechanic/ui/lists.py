6dba52884f2dc6c776bb2c55edde4579939a83db
Jack Jennings <j@ckjennin.gs>
2014-12-11 14:36:48 -0800

    extract description formatter into module

--- src/lib/mechanic/ui/lists.py
+++ src/lib/mechanic/ui/lists.py
@@ -4,7 +4,8 @@ from mojo.extensions import ExtensionBundle
 
 from mechanic.update import Update
 from mechanic.storage import Storage
-from mechanic.ui.formatter.version import VersionFormatter
+from mechanic.ui.formatters.version import VersionFormatter
+from mechanic.ui.formatters.description import DescriptionFormatter
 
 
 class ExtensionList(List):
@@ -76,7 +77,7 @@ class InstallationList(List):
                     "key": "extension",
                     "width": 200,
                     "editable": False,
-                    "formatter": ExtensionDescriptionFormatter.alloc().init()}]
+                    "formatter": DescriptionFormatter.alloc().init()}]
 
         super(InstallationList, self).__init__(posSize,
                                                [],
@@ -156,49 +157,3 @@ def InstalledIndicator():
         image = NSImage.imageNamed_('installedIndicator')
 
     return image
-
-
-class ExtensionDescriptionFormatter(NSFormatter):
-
-    def stringForObjectValue_(self, obj):
-        if obj is None or isinstance(obj, NSNull):
-            return ''
-        return obj
-
-    def attributedStringForObjectValue_withDefaultAttributes_(self, obj, attrs):
-        attrs = dict(attrs)
-
-        paragraph = NSMutableParagraphStyle.alloc().init()
-        paragraph.setMinimumLineHeight_(20.0)
-        attrs[NSParagraphStyleAttributeName] = paragraph
-
-        string = NSMutableAttributedString.alloc().initWithString_attributes_('', attrs)
-
-        name = NSAttributedString.alloc().initWithString_attributes_(obj['name'] or '', attrs)
-        string.appendAttributedString_(name)
-
-        attrs[NSForegroundColorAttributeName] = NSColor.colorWithCalibratedWhite_alpha_(0.6, 1)
-
-        space = NSAttributedString.alloc().initWithString_attributes_(u'\u2003', attrs)
-        string.appendAttributedString_(space)
-
-        author = NSAttributedString.alloc().initWithString_attributes_(obj['author'] or '', attrs)
-        string.appendAttributedString_(author)
-
-        paragraph = NSMutableParagraphStyle.alloc().init()
-        paragraph.setLineBreakMode_(NSLineBreakByTruncatingTail)
-        paragraph.setMaximumLineHeight_(14.0)
-
-        attrs[NSParagraphStyleAttributeName] = paragraph
-        attrs[NSFontAttributeName] = NSFont.systemFontOfSize_(10.0)
-
-        cr = NSAttributedString.alloc().initWithString_attributes_('\n', attrs)
-        string.appendAttributedString_(cr)
-
-        description = NSAttributedString.alloc().initWithString_attributes_(obj['description'] or u'\u2014', attrs)
-        string.appendAttributedString_(description)
-
-        return string
-
-    def objectValueForString_(self, string):
-        return string
