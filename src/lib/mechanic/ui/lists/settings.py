b583251fffc252f1cb8c0c1e7c0c6b3dfd025f81
Jack Jennings <j@ckjennin.gs>
2014-12-11 19:14:10 -0800

    add setting check box and save callback for setting list

--- src/lib/mechanic/ui/lists/settings.py
+++ src/lib/mechanic/ui/lists/settings.py
@@ -1,5 +1,6 @@
 from vanilla import CheckBoxListCell
 
+from mechanic.storage import Storage
 from mechanic.extension import Extension
 from mechanic.ui.lists.extension import ExtensionList
 from mechanic.ui.formatters.version import VersionFormatter
@@ -14,5 +15,16 @@ class SettingsList(ExtensionList):
 
     def __init__(self, posSize, **kwargs):
         kwargs['columnDescriptions'] = self.__columns
+        kwargs['editCallback'] = self.save
         configured = [e for e in Extension.all() if e.is_configured]
         super(SettingsList, self).__init__(posSize, configured, **kwargs)
+
+    def save(self, sender):
+        ignore = Storage.get("ignore")
+        for row in self.get():
+            if not row["check_for_updates"]:
+                ignore[row["name"]] = True
+            elif row["name"] in ignore:
+                del ignore[row["name"]]
+        print ignore
+        Storage.set('ignore', ignore)
--- src/lib/mechanic/ui/tabs/settings.py
+++ src/lib/mechanic/ui/tabs/settings.py
@@ -2,9 +2,9 @@ from AppKit import *
 from vanilla import *
 from vanilla.dialogs import getFile
 
-from mechanic.storage import Storage
 from mechanic.ui.lists.settings import SettingsList
 from mechanic.ui.tabs.base import BaseTab
+from mechanic.ui.fields.setting_check_box import SettingCheckBox
 
 
 class SettingsTab(BaseTab):
@@ -14,35 +14,14 @@ class SettingsTab(BaseTab):
 
     updates_label = "Check for updates on startup"
     minor_updates_label = "Ignore patch updates on startup"
-    check_on_startup = Storage.get("check_on_startup")
 
-    def setup(self):                
-        self.list = SettingsList((0, 60, -0, -0),
-                                 editCallback=self.update)
+    def setup(self):
+        self.list = SettingsList((0, 60, -0, -0))
 
-        self.checkForUpdates = CheckBox((0, 0, -0, 0),
-                                        self.updates_label,
-                                        value=Storage.get("check_on_startup"),
-                                        callback=self.saveCheckForUpdates)
+        self.startup = SettingCheckBox((0, 0, -0, 0),
+                                       self.updates_label,
+                                       "check_on_startup")
 
-        self.ignorePatchUpdates = CheckBox((0, 25, -0, 0),
-                                           self.minor_updates_label,
-                                           value=Storage.get("ignore_patch_updates"),
-                                           callback=self.saveIgnorePatchUpdates)
-
-    def saveCheckForUpdates(self, sender):
-        Storage.set("check_on_startup", 
-            bool(self.checkForUpdates.get()))
-
-    def saveIgnorePatchUpdates(self, sender):
-        Storage.set("ignore_patch_updates", 
-            bool(self.ignorePatchUpdates.get()))
-
-    def update(self, sender):
-        ignore = Storage.get("ignore")
-        for row in self.list.get():
-            if not row["check_for_updates"]:
-                ignore[row["name"]] = True
-            elif row["name"] in ignore:
-                del ignore[row["name"]]
-        Storage.set('ignore', ignore)
+        self.patches = SettingCheckBox((0, 25, -0, 0),
+                                       self.minor_updates_label,
+                                       "ignore_patch_updates")
