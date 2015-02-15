f01c342b45a2b3374b1da6df04655e4eec233450
Jack Jennings <j@ckjennin.gs>
2015-02-14 16:58:58 -0800

    cooler logging module

--- src/lib/mechanic/logger.py
+++ src/lib/mechanic/logger.py
@@ -1,5 +1,23 @@
-from __future__ import print_function
+import sys
+import logging
+
+from mechanic import env
 
 
-def info(text):
-    print("Mechanic: {}".format(text))
+logger = logging.getLogger('Mechanic')
+logger.setLevel(getattr(logging, env.log_level.upper(), logging.DEBUG))
+
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
+formatting = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(formatting)
+handler.setFormatter(formatter)
+
+debug    = logger.debug
+info     = logger.info
+warn     = logger.warn
+error    = logger.error
+critical = logger.critical
+
+info('Logger set to level %s', logging.getLevelName(logger.level))
