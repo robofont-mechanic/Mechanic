48130dd9c5caa4fe763da21cd4bb4a821d799e5c
Jack Jennings <j@ckjennin.gs>
2015-02-16 12:31:24 -0800

    fix demo script

--- bin/demo.py
+++ bin/demo.py
@@ -2,11 +2,11 @@ from mechanic.registry import Registry
 from mechanic.ui.windows.main import MechanicWindow
 from mechanic.ui.windows.notification import UpdateNotificationWindow
 
-window = MechanicWindow('install')
+window = MechanicWindow().open('install')
 tab = window.current_tab.view
-table = tab.content.list
+table = tab.list
 
-table.set(Registry('http://www.robofontmechanic.com').all())
+table.set(Registry('http://www.robofontmechanic.com').extensions())
 tab.enable()
 
 def dummy_updates(self, *args, **kwargs): return ['updates']
