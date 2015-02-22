1489f1e2684c311a546a90dcd590a6c00adda8b2
Jack Jennings <j@ckjennin.gs>
2015-02-22 10:53:39 -0800

    catch malformed version numbers before throwing error. #8

--- src/lib/mechanic/version.py
+++ src/lib/mechanic/version.py
@@ -1,10 +1,19 @@
 import re
 import semversion
 
+from mechanic import logger
+
 
 _re = re.compile('^'
                  '(\d+)\.(\d+)'  # minor, major
                  '$')
+
+
+_error = """Version number `%s` is not in a format Mechanic understands.
+    Version numbers should conform to the spec described on http://semver.org
+    with the exception that it is also possible to use only minor and major
+    version numbers in accordance with the RoboFont extension spec. You may
+    use pre-release version numbers in the format 1.0.0-alpha, 0.2.1-beta, etc."""
 
 
 class Version(semversion.Version):
@@ -17,4 +26,9 @@ class Version(semversion.Version):
         version = str(version)
         if _re.match(version):
             version += '.0'
-        super(Version, self).__init__(version)
+
+        try:
+            super(Version, self).__init__(version)
+        except semversion.VersionError:
+            logger.warn(_error, version)
+            super(Version, self).__init__('0.0.0')
